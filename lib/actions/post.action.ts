ba69f96cde85741189474e8c85e0fb834cf5f7b5
22520592 <22520592@gm.uit.edu.vn>
2024-11-14 23:53:54 +0700

    like & dislike post

--- lib/actions/post.action.ts
+++ lib/actions/post.action.ts
@@ -104,7 +104,7 @@ export async function updatePost(
 
 export async function likePost(
   postId: String | undefined,
-  userId: String | undefined
+  userId: Schema.Types.ObjectId | undefined
 ) {
   try {
     connectToDatabase();
@@ -119,13 +119,36 @@ export async function likePost(
       throw new Error(`User with ID ${userId} does not exist.`);
     }
 
-    await post.likedIds.addToSet(userId);
+    await post.likes.addToSet(userId);
 
     await post.save();
 
     return { message: `Liked post ${postId}` };
   } catch (error) {
     console.error(error);
+    throw error;
+  }
+}
+
+export async function disLikePost(
+  postId: string | undefined,
+  userId: Schema.Types.ObjectId | undefined
+) {
+  try {
+    connectToDatabase();
+    const post = await Post.findById(postId);
+    const user = await User.findById(userId);
+    if (!post || !user) {
+      throw new Error("Your required content does not exist!");
+    }
+
+    await post.likes.pull(userId);
+
+    await post.save();
+
+    return { message: `Disliked post ${postId}` };
+  } catch (error) {
+    console.log(error);
     throw error;
   }
 }
