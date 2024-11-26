82973b3bad9dac5a924cd009839b00552e2e21b1
22520592 <22520592@gm.uit.edu.vn>
2024-11-26 21:26:42 +0700

    create media

--- lib/actions/media.action.ts
+++ lib/actions/media.action.ts
@@ -1,4 +1,3 @@
-import cloudinary from "@/cloudinary";
 import { connectToDatabase } from "../mongoose";
 import Media from "@/database/media.model";
 import { MediaCreateDTO, MediaResponseDTO } from "@/dtos/MediaDTO";
@@ -14,57 +13,23 @@ export async function getAllComments(): Promise<MediaResponseDTO[]> {
   }
 }
 
-// export async function createMedia(
-//   params: MediaCreateDTO,
-//   createBy: mongoose.Schema.Types.ObjectId | undefined
-// ): Promise<MediaResponseDTO> {
-//   try {
-//     await connectToDatabase();
-//     const mediaData = {
-//       url: params.url,
-//       type: params.type,
-//       caption: params.caption || "",
-//       createdAt: new Date(),
-//       createdBy: createBy || new mongoose.Types.ObjectId(),
-//       shares: [],
-//       likes: [],
-//       comments: [],
-//     };
-
-//     const newMedia = await Media.create(mediaData);
-//     return newMedia as MediaResponseDTO;
-//   } catch (error) {
-//     console.error("Error creating media:", error);
-//     throw error;
-//   }
-// }
-
 export async function createMedia(
   params: MediaCreateDTO,
   createBy: mongoose.Schema.Types.ObjectId | undefined
 ): Promise<MediaResponseDTO> {
   try {
     await connectToDatabase();
-
-    const file = params.url;
-
-    if (!file) {
-      throw new Error("No file provided");
-    }
-    const result = await cloudinary.uploader.upload(file, {
-      folder: "Media",
-    });
+    console.log(createBy);
 
     const mediaData = {
-      url: result.secure_url,
+      url: params.url,
       type: params.type,
       caption: params.caption || "",
-      createdAt: new Date(),
-      createdBy: createBy || new mongoose.Types.ObjectId(),
+      createAt: new Date(),
+      createBy: createBy || new mongoose.Types.ObjectId(),
       shares: [],
       likes: [],
       comments: [],
-      publicId: result.public_id,
     };
 
     const newMedia = await Media.create(mediaData);
