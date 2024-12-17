d62b27654e760b74feb80e8e04ac2b4eec86fc1a
GiaBaoAnh04 <giabaoanh3003@gmail.com>
2024-12-18 01:03:41 +0700

    change report modal and fix delete report comment

--- dtos/reportDTO.ts
+++ dtos/reportDTO.ts
@@ -23,6 +23,18 @@ export interface ReportCreateDTO {
   proofs?: string[]; // Các bằng chứng (tuỳ chọn)
 }
 
+export interface CommentReportCreateDTO {
+  title?: string; // Tiêu đề báo cáo (tuỳ chọn)
+  content: string; // Nội dung báo cáo
+  createdById: Schema.Types.ObjectId;
+  reportedId: Schema.Types.ObjectId; // ID của người bị báo cáo
+  reportedEntityId: Schema.Types.ObjectId; // ID của thực thể bị báo cáo (Post ID, User ID, etc.)
+  parentReportEntityId: string;
+  entityType: string; // Loại thực thể (e.g., "post", "user", etc.)
+  attachments?: string[]; // Các file đính kèm (tuỳ chọn)
+  proofs?: string[]; // Các bằng chứng (tuỳ chọn)
+}
+
 export interface ReportResponseDTO {
   _id: string;
   title?: string;
@@ -30,20 +42,7 @@ export interface ReportResponseDTO {
   createdById: UserInfor;
   reportedId: UserInfor; // Thêm reportedId vào đây
   reportedEntityId: string;
-  entityType: string;
-  status: number; // Trạng thái báo cáo (e.g., "pending", "resolved", etc.)
-  createdAt: Date;
-  attachments?: string[]; // Các file đính kèm (nếu có)
-  proofs?: string[]; // Các bằng chứng (nếu có)
-}
-
-export interface ReportCommentResponseDTO {
-  _id: string;
-  title?: string;
-  content: string;
-  createdById: UserInfor;
-  reportedId: UserInfor; // Thêm reportedId vào đây
-  reportedEntityId: string;
+  parentReportEntityId?: string;
   entityType: string;
   status: number; // Trạng thái báo cáo (e.g., "pending", "resolved", etc.)
   createdAt: Date;
